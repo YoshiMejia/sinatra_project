aaac4989b0e70589c346c7a374e54dacbd4cd847
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-21 15:56:02 -0400

    created CRUD routes for plant controller

--- community_gardens/app/controllers/plants_controller.rb
+++ community_gardens/app/controllers/plants_controller.rb
@@ -13,22 +13,71 @@ class PlantsController < ApplicationController
             @plant = Plant.create(name: params[:name], description: params[:info], user_id: session[:user_id])
             @plant.save
             @user = User.find_by(id: @plant.user_id)
+            @plants = @user.plants
             erb :'/plants/index' #"garden"
         end
     end
 
     #read - view plant page and/or plants
-    get '/plants' do
+    get '/plants' do #view garden 
         redirect_if_not_logged_in
         @user = User.find_by(id: session[:user_id])
-#show page for viewing one plant? index shows all plants, each one has a link to 'plant profile' which shows if any other users have the same plant? plant.find_by(name: plant_name) then iterate thru and show all User names w that plant.
+        @plants = @user.plants
         erb :'/plants/index'
     end
 
     #edit - edit the user's plants
+    #this can be a link from the index view, that way it automatically grabs just one id
 
+    get '/plants/:id' do #patch redirects here
+        redirect_if_not_logged_in
+        # binding.pry
+        @user = User.find(session[:user_id])
+        @plants = @user.plants 
+        @plant = Plant.find_by(id: params[:id])
+        if @plant.user == @user
+            erb :'/plants/show'
+        else
+            erb :'/errors/user_error'
+        end
+    end
 
-    #delete - delete the user's plants (checkbox form?)
+    get '/plants/:id/edit_plant' do #view form for editing
+        redirect_if_not_logged_in
+        @user = User.find(session[:user_id])
+        @plant = Plant.find_by(params[:id])
+        # binding.pry
+        if @plant.user == @user
+            erb :'/plants/edit'
+        else
+            erb :'/errors/user_error'
+        end
+    end
+
+    patch '/plants/:id' do
+        redirect_if_not_logged_in
+        @plant = Plant.find(params[:id]) 
+        @user = User.find(session[:user_id])
+        if @plant.user == @user
+            @plant.update(params["plant"])
+            @plant.save
+            redirect "/plants/#{@plant.id}"
+        else
+            erb :'/errors/user_error'
+        end
+    end
+
+    delete '/plants/:id/delete_garden' do
+        @user = User.find(session[:user_id])
+        @user.plants.clear
+        redirect "users/#{@user.id}/homepage"
+    end
+
+    delete '/plants/:id/delete_plant' do
+        @plant = Plant.find(params[:id]) 
+        @plant.destroy
+        redirect '/plants'
+    end 
     
     
 end #class ender
