939b42ea867d611d75811e18249c6af2233b775e
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-22 16:28:36 -0400

    changed password attribute to password_digest in order to accomodate for the bcrypt gem

--- community_gardens/app/models/user.rb
+++ community_gardens/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :plants
-    validates_presence_of :name, :email, :password
+    validates_presence_of :name, :email, :address, :password_digest
     has_secure_password
 end
